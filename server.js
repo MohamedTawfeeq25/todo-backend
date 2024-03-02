03196f87c5f6f3a102e943cdf950c5710ace74ac
SMT <mohamedtawfeeqprogrammer25@gmail.com>
2024-03-02 12:46:15 +0530

    login api

--- server.js
+++ server.js
@@ -23,14 +23,14 @@ app.get('/test',(req,res)=>{
 app.post('/to-do/auth/register',(req,res)=>{
     // checks whether the user exists 
     sql.query("SELECT u_id FROM users WHERE email=?",[req.body.email],(err2,ret)=>{
-        if(err==null){
+        if(err2==null){
             if(ret.length==0){
                 //password encryption
                 bcrypt.hash(req.body.password,10,(err1,hash)=>{
-                    if(err==null){
+                    if(err1==null){
                         //adding user to table
                        sql.query("INSERT INTO users(username,email,password,phone) VALUES(?,?,?,?);",[req.body.username,req.body.email,hash,req.body.phone],(err,out)=>{
-                           if(err==null){
+                           if(err2==null){
                                if(out.affectedRows==1){
                                    //user added
                                    res.send({message:"User added"});
@@ -64,4 +64,62 @@ app.post('/to-do/auth/register',(req,res)=>{
 
     
 });
+app.post('/to-do/auth/login',(req,res)=>{
+    if(req.body.email!=undefined){
+        sql.query("SELECT password FROM users WHERE email=?;",[req.body.email],(err1,out1)=>{
+            if(err1==null){
+                if(out1.length==0){
+                    console.log("user not exits");
+                    res.send({message:"user not exits"});
+                }
+                else{
+                    bcrypt.compare(req.body.password,out1[0].password,(err,ret)=>{
+                        if(err==null){
+                            if(ret===true){
+                                res.send({message:"verfied"});
+                            }
+                            else{
+                                res.send({message:"incorrect password"});
+                            }
+                        }
+                        else{
+                            console.log(err);
+                        }
+                    })
+                }
+            }
+            else{
+                console.log(err1);
+            }
+        })
+    }
+    else if(req.body.phone!=undefined){
+        sql.query("SELECT password FROM users WHERE phone=?;",[req.body.phone],(err1,out1)=>{
+            if(err1==null){
+                if(out1.length==0){
+                    console.log("user not exits");
+                    res.send({message:"user not exits"});
+                }
+                else{
+                    bcrypt.compare(req.body.password,out1[0].password,(err,ret)=>{
+                        if(err==null){
+                            if(ret===true){
+                                res.send({message:"verfied"});
+                            }
+                            else{
+                                res.send({message:"incorrect password"});
+                            }
+                        }
+                        else{
+                            console.log(err);
+                        }
+                    })
+                }
+            }
+            else{
+                console.log(err1);
+            }
+        })
+    }
+})
 app.listen(2000,()=>{console.log("server started")});
